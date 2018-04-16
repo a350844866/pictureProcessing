d43a39b798aebba3481f7e02d1bd9f51011d8cb5
a350844866 <350844866@qq.com>
2018-04-16 14:34:33 +0800

    Initial commit

---
+++ src/main/java/com/xuexue/SpringBoot.java
@@ -0,0 +1,15 @@
+package com.xuexue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by LYJ on 2017/10/17.
+ */
+@SpringBootApplication
+public class SpringBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot.class,args);
+    }
+}
---
+++ src/main/java/com/xuexue/controller/UploadController.java
@@ -0,0 +1,21 @@
+package com.xuexue.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author jiaxu
+ *
+ * @version $Id: uploadController.java, v 0.1 2018/4/16 12:24 jiaxu Exp $$
+ */
+
+public class UploadController {
+
+    @RequestMapping("/uploadPicture")
+    public String upload() {
+
+
+        return "";
+    }
+
+}
